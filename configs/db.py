98cfd06213a1f737f6316eb05ad51aafa996c7d1
Dev Arora <dev.arora@acadecraft.com>
2025-07-28 12:49:12 +0530

    update db string

--- configs/db.py
+++ configs/db.py
@@ -2,7 +2,8 @@
 from pymongo import MongoClient
 from configs.logger import log
 
-client = MongoClient('mongodb://developer:ah6M6vIz52YYJzy1@3.109.96.163:27017/e-finder?authSource=e-finder&readPreference=primary&serverSelectionTimeoutMS=20000&appname=mongosh%201.6.1&directConnection=true&ssl=false')
+# client = MongoClient('mongodb://developer:ah6M6vIz52YYJzy1@3.109.96.163:27017/e-finder?authSource=e-finder&readPreference=primary&serverSelectionTimeoutMS=20000&appname=mongosh%201.6.1&directConnection=true&ssl=false')
+client = MongoClient('mongodb+srv://admin:jf0AY6Gw1vfnt5sn@cluster0.lgbepit.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 log.info("Connected to MongoDB")
 db = client["e-finder"]
 users = db["users-1"]
