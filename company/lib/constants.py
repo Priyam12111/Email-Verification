ac85ea93d2a32253b49e0a521e46579fc8095df7
Dev Arora <dev.arora@acadecraft.com>
2025-10-07 11:38:28 +0530

    db update

--- company/lib/constants.py
+++ company/lib/constants.py
@@ -1,13 +1,14 @@
 # define constant variables
 
-COLLECTION_COMPANY = 'company'
-COLLECTION_USER = 'users'
+COLLECTION_COMPANY = 'company-1'
+COLLECTION_USER = 'users-1'
 COLLECTION_URL = 'urls'
 COLLECTION_CREDENTIAL = 'credentials'
 COLLECTION_GMAIL_CREDENTIAL = 'gmail-credential'
 COLLECTION_MAIL_TEMPLATE = 'mail-templates'
 COLLECTION_SYSTEM = 'systems'
-DB_CONNECTION_STRING = 'mongodb+srv://developer:sXk5MQfvGZ4GZC3h@cluster0.0bwi4vm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+# DB_CONNECTION_STRING = 'mongodb+srv://developer:sXk5MQfvGZ4GZC3h@cluster0.0bwi4vm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+DB_CONNECTION_STRING = 'mongodb://admin:AdminStrongPass123@14.195.222.181:8989'
 DB_NAME = 'e-finder'
 GMAIL_MESSAGE_SEND_LIMIT=1000
 # var
