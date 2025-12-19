3421cfa5b54c5fc032ff7a058f4b8c063c425596
Priyam Tomar <priyamtomar012@gmail.com>
2025-12-19 14:07:11 +0530

    req update

--- company/li_company_finder.py
+++ company/li_company_finder.py
@@ -1,8 +1,14 @@
 from consumer import *
+import pymongo
 
+client = pymongo.MongoClient(
+    "mongodb://admin:AdminStrongPass123@14.195.222.181:8989/admin"
+)
+db = client["e-finder"]["company-1"]
 if __name__ == "__main__":
+    db.find_one({"name_lc": re.compile("^flint international", re.IGNORECASE)})
+    # with open("company\\formatter.csv", "r") as f:
+    #     data = f.read()
+    # sample_data = list(data.splitlines())
 
-    with open("company\\formatter.csv", "r") as f:
-        data = f.read()
-    sample_data = list(data.splitlines())
-    li_page = print(process(sample_data, 1))
+    # li_page = print(process(sample_data, 1))
